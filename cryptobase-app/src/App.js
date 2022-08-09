9d828ea717dbcd67c604d2688f330e08a839915c
Ayush451 <88429944+Ayush451@users.noreply.github.com>
2022-08-09 11:05:43 +0100

    New Component and Routes
    Added routes and coinsearch component for home component

--- cryptobase-app/src/App.js
+++ cryptobase-app/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { ThemeProvider } from './context/ThemeContext';
+import Home from './routes/Home';
+import Signin from './routes/Signin';
+import Signup from './routes/Signup';
+import Account from './routes/Account';
+import Axios from 'axios';
+
 
 
 function App() {
+  const [coins, setCoins] = useState();
   return (
     <ThemeProvider>
      <Navbar/>
+     <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/account' element={<Account/>}/>
+     </Routes>
     </ThemeProvider>
   );
 }
