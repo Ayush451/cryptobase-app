69c0da547c579ad4caa2d017785cc48f803fd9f0
Ayush451 <88429944+Ayush451@users.noreply.github.com>
2022-08-10 03:03:08 +0100

    new Component
    Added styling and dymanic data  to CoinPage and Coinitem components

--- cryptobase-app/src/App.js
+++ cryptobase-app/src/App.js
@@ -6,8 +6,10 @@ import Home from './routes/Home';
 import Signin from './routes/Signin';
 import Signup from './routes/Signup';
 import Account from './routes/Account';
+import CoinPage from './routes/CoinPage'
 
 import axios from 'axios';
+import Footer from './components/Footer';
 
 
 
@@ -30,7 +32,12 @@ function App() {
         <Route path='/signin' element={<Signin/>}/>
         <Route path='/signup' element={<Signup/>}/>
         <Route path='/account' element={<Account/>}/>
+
+        <Route path='/coin/:coinId' element={<CoinPage/>}>
+          <Route path=':coinId'/>
+        </Route>
      </Routes>
+     <Footer/>
     </ThemeProvider>
   );
 }
